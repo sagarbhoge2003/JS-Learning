c198f60d9559901736900a770ece796107c30566
Sagar Bhoge <sagarbhoge2003@gmail.com>
2024-06-29 18:48:40 +0530

    variables & datatypes

---
+++ 01-Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3) we are using node js not browser
+
+//console.log(3+3);
+
+let name = "sagar";
+let age=20
+let isTrue=false
+
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => empty value
+//undefined => not defined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof name);
+console.log(typeof null);
---
+++ 01-Basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=111 
+var accountPassword="1234"
+let city="Pune"
+accountEmail="sagar@gmail.com" //do not use
+let accountState;
+//prefer not to use var
+//console.log(accountId);
+console.table([accountId,accountPassword,city,accountEmail,accountState])
