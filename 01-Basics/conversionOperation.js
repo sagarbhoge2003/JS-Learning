c8549faefd5e5b69eecd1f4ce65bc9aac6a07909
Sagar Bhoge <sagarbhoge2003@gmail.com>
2024-06-30 19:13:17 +0530

    conversion

---
+++ 01-Basics/conversionOperation.js
@@ -0,0 +1,35 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+let name = "sagar"
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "sagar"
+
+
+let booleanLoggedIn = Boolean (isLoggedIn)
+console.log(booleanLoggedIn);
+
+console.log(typeof booleanLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "sagar" => true
+
+
+let numbers = 111
+
+let stringNumber = String(numbers)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
