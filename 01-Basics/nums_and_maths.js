69e66a66250c3d5b45b4770618a691fae05f23f2
Sagar Bhoge <sagarbhoge2003@gmail.com>
2024-07-05 11:12:16 +0530

    nums_&_maths

---
+++ 01-Basics/nums_and_maths.js
@@ -0,0 +1,14 @@
+const score = 100;
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log((balance.toFixed(2)));
+
+const otherNum = 23.659
+
+console.log(otherNum.toPrecision(3));
